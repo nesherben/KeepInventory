110f978bdc001ec5d8757934d0f2f433066ec5cb
Nestor Hernandez <nestor8299@gmail.com>
2026-08-31 12:54:41 +0200

    Refactor code structure for improved readability and maintainability

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,19 +15,13 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.keepinventory"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+    // Asegúrate de mantener tu applicationId original si era distinto
+    applicationId = "com.example.keepinventory" 
+    minSdk = flutter.minSdkVersion // Listo para SQLite
+    targetSdk = flutter.targetSdkVersion
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
+}
 
     buildTypes {
         release {
